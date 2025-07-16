d328c1ec002696a361d7835bbd4cee334bb65441
astroenglert <astroenglert@gmail.com>
2025-07-16 13:56:15 -0400

    updating coadd_3b template for multiband

--- processing_step_templates/coadd3b_template.sh
+++ processing_step_templates/coadd3b_template.sh
@@ -4,9 +4,9 @@
 #SBATCH --mem=50GB
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=4
-#SBATCH -J coadd_3b_process_band_cluster_name
-#SBATCH -o slurm_outputs/coadd_3b_process_band_cluster_name-%j.out
-#SBATCH -e slurm_outputs/coadd_3b_process_band_cluster_name-%j.err
+#SBATCH -J coadd_3b_process_cluster_name
+#SBATCH -o slurm_outputs/coadd_3b_process_cluster_name-%j.out
+#SBATCH -e slurm_outputs/coadd_3b_process_cluster_name-%j.err
 
 # defining variables
 # TEXT REPLACED FOR TEMPLATES
@@ -14,7 +14,6 @@ CLN="cluster_name"
 LOAD_LSST="load_pipeline_path"
 CLUSTER_DIR="cluster_dir" # UPDATE LATER
 PY_SCRIPTS="py_scripts"
-BAND=process_band
 
 # navigate to .../cluster_name
 cd ${CLUSTER_DIR}
@@ -32,14 +31,13 @@ export MKL_NUM_THREADS=1
 
 # build the pipeline
 pipetask build -p DRP-LoVoCCS.yaml#step3b \
-    -C measure:${CLUSTER_DIR}/configs/coadd_3b_config_${BAND}.py \
     -s ${CLUSTER_DIR}/pipeline_yamls/DRP_step3b.yaml
 
 # step3b measure
 bps submit -b repo/repo \
-    -i DECam/processing/quality_detectors_${BAND},DECam/processing/final_visit_summary_${BAND},DECam/processing/visit_summary_${BAND},DECam/processing/coadd_3a,refcats,skymaps \
-    -o DECam/processing/coadd_3b_${BAND} \
+    -i DECam/processing/coadd_3a,refcats,skymaps \
+    -o DECam/processing/coadd_3b \
     -p ${CLUSTER_DIR}/pipeline_yamls/DRP_step3b.yaml \
-    -d "instrument='DECam' AND skymap='${CLN}_skymap' AND band='${BAND}'" \
+    -d "instrument='DECam' AND skymap='${CLN}_skymap'" \
     ${CLUSTER_DIR}/configs/bps_config_coadd3b.yaml
 
